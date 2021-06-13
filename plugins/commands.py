37902f1d4be67eac7ba2b3e3364a33f8f7b37f2e
njanunni777 <85037210+njanunni777@users.noreply.github.com>
2021-06-13 18:12:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,8 +247,8 @@ async def help(client, message):
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("JOIN GROUP", url="https://t.me/cinemuseum2"),
+                    InlineKeyboardButton("Devoloper 🌞", url="https://t.me/G9037944937")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/njanunni777/Cine-Filter-Bot")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
